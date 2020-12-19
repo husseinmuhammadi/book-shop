80a11830e031817e583ee03dc1c3f86e512c70b1
Hossein Mohammadi <hossein.mohammadi@outlook.com>
2020-12-19 20:21:02 +0330

    Book Store

---
+++ repository/src/test/java/com/toranj/fintech/books/repository/ProductRepositoryIntegrationTest.java
@@ -0,0 +1,14 @@
+package com.toranj.fintech.books.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class ProductRepositoryIntegrationTest {
+    @Test
+    void given_when_then() {
+
+    }
+}
---
+++ repository/src/test/java/com/toranj/fintech/books/to/ProductsTest.java
@@ -0,0 +1,7 @@
+package com.toranj.fintech.books.to;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductsTest {
+
+}
